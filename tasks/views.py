dfb7833a176828e68a9f956d78c33cbe4deb57d2
spriyanshu200304 <priyanshu.shukla@expoundtechnivo.com>
2025-03-16 21:33:08 +0530

    fix api function

--- tasks/views.py
+++ tasks/views.py
@@ -16,7 +16,7 @@ redis_client = redis.StrictRedis(host='127.0.0.1', port=6379, db=1, decode_respo
 # Create Task
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def create_task():
+def create_task(request):
     try:
         data = json.loads(request.body)
 
